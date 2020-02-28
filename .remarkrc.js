064e369a07eb7e5450ca544e6ccd430a48be37e4
Matt Travi <programmer@travi.org>
2020-02-28 01:15:52 -0600

    docs(remark): configured remark formatting

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,3 +1,13 @@
+exports.settings = {
+  listItemIndent: 1,
+  emphasis: '_',
+  strong: '_',
+  bullet: '*',
+  incrementListMarker: false
+};
+
 exports.plugins = [
-  '@form8ion/remark-lint-preset'
+  '@form8ion/remark-lint-preset',
+  [require('remark-toc'), {tight: true}],
+  ['remark-usage', {heading: 'example'}]
 ];
