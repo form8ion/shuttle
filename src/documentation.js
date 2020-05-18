41577b21658ab9a929ece32f598f66129ab1e6f2
Matt Travi <programmer@travi.org>
2020-05-17 21:14:00 -0500

    feat(remark): started processing the readme
    although no plugins are applied yet

--- src/documentation.js
+++ src/documentation.js
@@ -1,3 +1,22 @@
-export default function () {
+import fs from 'fs';
+import {info} from '@travi/cli-messages';
+import * as remarkConfig from '../.remarkrc';
+import remark from '../thirdparty-wrappers/remark';
 
+export default function ({projectRoot}) {
+  info('Shuttling Documentation');
+
+  const pathToReadme = `${projectRoot}/README.md`;
+
+  return new Promise((resolve, reject) => {
+    remark()
+      .data('settings', remarkConfig.settings)
+      .process(fs.readFileSync(pathToReadme, 'utf8'), (err, file) => {
+        if (err) reject(err);
+        else {
+          fs.writeFileSync(pathToReadme, file);
+          resolve();
+        }
+      });
+  });
 }
